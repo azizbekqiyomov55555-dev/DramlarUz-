a058174dc4bce292473fe82ea2d30523c6014b5a
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-05-05 02:30:59 +0500

    Update and rename botku to bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from telegram.ext import (
 )
 
 # ─── KONFIGURATSIYA ────────────────────────────────────────
-BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8723400610:AAFm6t6DwI9ycG3lLgfOLWKdwh-02ZaVTUw"
+BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8723400610:AAHtJWYXYw0ObIpkiy34Do5x4f1TzmRbSeI"
 ADMIN_ID   = int(os.environ.get("ADMIN_ID") or "8537782289")
 
 JSONBLOB_URL = os.environ.get("JSONBLOB_URL") or "https://jsonblob.com/api/jsonBlob/019df4aa-10b1-725c-83f5-9901ab2db9b6"
