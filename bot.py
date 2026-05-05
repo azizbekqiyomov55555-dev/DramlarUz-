76bcb283c9782f509392102d42dd88665a80e2ae
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-05-05 18:16:50 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ from telegram.ext import (
 )
 
 # ─── KONFIGURATSIYA ────────────────────────────────────────
-BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8723400610:AAGqjQUbm-o_YVx5-J4_iQTMOMjggMdJ3RY"
+BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8723400610:AAFqiANuNpxKRXIJZQk0WnMc89Hxb4s6aQE"
 ADMIN_ID   = int(os.environ.get("ADMIN_ID") or "8537782289")
 
 JSONBLOB_URL      = os.environ.get("JSONBLOB_URL") or "https://jsonblob.com/api/jsonBlob/019df4aa-10b1-725c-83f5-9901ab2db9b6"
